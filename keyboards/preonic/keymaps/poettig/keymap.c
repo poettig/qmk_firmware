3ab3fa38ce552439f8c71c8e4286ae2a1d4d6b44
Peter Oettig <peet1993@gmail.com>
2024-02-14 18:52:02 +0100

    Add layout switch hotkey

--- keyboards/preonic/keymaps/poettig/keymap.c
+++ keyboards/preonic/keymaps/poettig/keymap.c
@@ -78,11 +78,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_LOWER] = LAYOUT_preonic_grid(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
-        _______, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,  ___X___, KC_DEL,
-        _______, ___X___, KC_MINS, KC_EQL,  ___X___, ___X___, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_BSPC, KC_PSCR,
-        _______, ___X___, ___X___, ___X___, ___X___, ___X___, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   ___X___, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD,  KC_VOLU, KC_MPLY
+        _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
+        _______, ___X___,       ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,  ___X___, KC_DEL,
+        _______, ___X___,       KC_MINS, KC_EQL,  ___X___, ___X___, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_BSPC, KC_PSCR,
+        _______, LCTL(KC_RCTL), ___X___, ___X___, ___X___, ___X___, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   ___X___, _______,
+        _______, _______,       _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD,  KC_VOLU, KC_MPLY
     ),
 
     [_RAISE] = LAYOUT_preonic_grid(
